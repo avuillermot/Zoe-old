392d3c8d3d7f1b2b958e6e72a19547b1f6d4a7b3
alexandre.vuillermot <alexandre.vuillermot@kuehne-nagel.com>
2020-10-09 16:34:16 +0200

    rebase test kmt

--- src/server.user.ts
+++ src/server.user.ts
@@ -4,7 +4,7 @@ import Query from "./user/user.serv";
 import ServerBase from "./server.base";
 
 /**
- * Allows to start an Express server (multiple)
+ * Allows to start an Express server (multiple2)
  */
 export default class ServerUser extends ServerBase {
 
